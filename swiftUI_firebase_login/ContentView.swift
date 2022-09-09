6b7499d0882679c22c614ee08e20d383e14fb9e0
m_mitin <maximus405@gmail.com>
2022-09-09 21:09:29 +0300

    adding buttons

--- swiftUI_firebase_login/ContentView.swift
+++ swiftUI_firebase_login/ContentView.swift
@@ -8,9 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black
+            
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .foregroundStyle(.linearGradient(colors: [.pink, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 1000, height: 400)
+                .rotationEffect(.degrees(135))
+                .offset(y: -350)
+            
+            VStack(spacing: 20){
+                Text("Welcome")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                    .offset(x: -100, y: -100)
+                
+                TextField("Email",text: $email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty) {
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty) {
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                Button {
+                    //
+                } label: {
+                    Text("Sign Up")
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(.linearGradient(colors: [.pink, .red], startPoint: .top, endPoint: .bottom))
+                        )
+                        .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y:100)
+                
+                Button {
+                    //
+                } label: {
+                    Text("Already have an account? Login")
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y: 110)
+
+
+                
+            }
+            .frame(width: 350)
+        }
+        .ignoresSafeArea()
     }
 }
 
@@ -18,4 +91,18 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+extension View {
+    func placeholder<Content: View>(
+         when shouldShow: Bool,
+         alignment: Alignment = .leading,
+         @ViewBuilder placeholder: () -> Content) -> some View {
+
+         ZStack(alignment: alignment) {
+             placeholder().opacity(shouldShow ? 1 : 0)
+             self
+         }
+     }
 }

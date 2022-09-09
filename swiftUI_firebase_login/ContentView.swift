5979f9ab0ecfa91994c046a5c6565fc25e36ac04
m_mitin <maximus405@gmail.com>
2022-09-09 20:54:44 +0300

    Initial Commit

---
+++ swiftUI_firebase_login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI_firebase_login
+//
+//  Created by Maxim Mitin on 9.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI_firebase_login/swiftUI_firebase_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_firebase_loginApp.swift
+//  swiftUI_firebase_login
+//
+//  Created by Maxim Mitin on 9.09.22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_firebase_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

9d0e30a57a82a87137a0ff81cf654426d22f040e
m_mitin <maximus405@gmail.com>
2022-09-09 22:19:02 +0300

    adding Data Model adding Data Manager

---
+++ swiftUI_firebase_login/DataManager.swift
@@ -0,0 +1,11 @@
+//
+//  DataManager.swift
+//  swiftUI_firebase_login
+//
+//  Created by Maxim Mitin on 9.09.22.
+//
+
+import SwiftUI
+import Firebase
+
+
---
+++ swiftUI_firebase_login/Dog.swift
@@ -0,0 +1,13 @@
+//
+//  Dog.swift
+//  swiftUI_firebase_login
+//
+//  Created by Maxim Mitin on 9.09.22.
+//
+
+import SwiftUI
+
+struct Dog: Identifiable {
+    var id: String
+    var Breed: String
+}

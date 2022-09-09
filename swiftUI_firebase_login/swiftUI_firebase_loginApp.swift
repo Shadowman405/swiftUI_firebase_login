17657bc95c61a0ae3ba24be30b6380173d04edf5
m_mitin <maximus405@gmail.com>
2022-09-09 21:54:23 +0300

    adding Firebase dependencies and configure AppDelegate

--- swiftUI_firebase_login/swiftUI_firebase_loginApp.swift
+++ swiftUI_firebase_login/swiftUI_firebase_loginApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct swiftUI_firebase_loginApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
